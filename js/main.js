4e51554f7722824ff882e97f1ee5352938d2cb4d
edoardo.davini <edoardo.davini@gmail.com>
2016-07-01 10:08:37 +0200

    just a first experiment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var init = function() {
     $.getJSON('data/data.json', function(data) {
         console.log(data);
+
+        generateData(data);
+
+        document.title = data.bio.greetings + " " + data.bio.name + " " + data.bio.surname;
     });
+};
+
+var generateData = function(data) {
+    for (var k in data) {
+        var source = $('#'+k+'-template').html();
+        var template = Handlebars.compile(source);
+
+        var html = template(data[k]);
+        $('#'+k).html(html);
+    }
 };
 
 
